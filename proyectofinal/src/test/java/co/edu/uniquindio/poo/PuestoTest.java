0416dc5014f97b246774484ea9f5f318cd0a687a
AlejandroPinedap <alejandro.pinedap@uqvirtual.edu.co>
2024-05-15 09:42:33 -0500

    prueba

--- proyectofinal/src/test/java/co/edu/uniquindio/poo/PuestoTest.java
+++ proyectofinal/src/test/java/co/edu/uniquindio/poo/PuestoTest.java
@@ -1,5 +1,16 @@
 package co.edu.uniquindio.poo;
 
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 public class PuestoTest {
-    private 
+        private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
+
+        @Test
+        public 
+
+
+
 }
