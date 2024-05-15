a3157dc4989803c3dab6aa8b025ce95548fba7e3
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2024-05-15 07:21:07 -0500

    carpeta completa
    se implemento la prueba

---
+++ proyectofinal/src/test/java/co/edu/uniquindio/poo/ParqueaderoTest.java
@@ -0,0 +1,39 @@
+package co.edu.uniquindio.poo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParqueaderoTest {
+    @Test
+    public void testRegistrarVehiculo() {
+        Parqueadero parqueadero = new Parqueadero(10);
+        Propietario propietario = new Propietario("1", "Juan Perez");
+        Vehiculo vehiculo = new Vehiculo("ABC123", "2022", propietario);
+        Puesto puesto = new Puesto(0, 0);
+
+        assertTrue(parqueadero.registrarVehiculo(vehiculo, puesto));
+        assertFalse(parqueadero.registrarVehiculo(vehiculo, puesto));
+    }
+
+    @Test
+    public void testCalcularCosto() {
+        Parqueadero parqueadero = new Parqueadero(10);
+        parqueadero.configurarTarifa("Carro", 10.0);
+
+        Propietario propietario = new Propietario("1", "Juan Perez");
+        Vehiculo vehiculo = new Vehiculo("ABC123", "2022", propietario);
+
+        assertEquals(50.0, parqueadero.calcularCosto(vehiculo, 5.0));
+    }
+
+    @Test
+    public void testConfigurarTarifa() {
+        Parqueadero parqueadero = new Parqueadero(10);
+        parqueadero.configurarTarifa("Carro", 10.0);
+
+        Propietario propietario = new Propietario("1", "Juan Perez");
+        Vehiculo vehiculo = new Vehiculo("ABC123", "2022", propietario);
+
+        assertEquals(10.0, parqueadero.calcularCosto(vehiculo, 1.0));
+    }
+}
